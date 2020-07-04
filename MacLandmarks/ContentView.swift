e9de7c4264ff2ef4f7837e99f6786fca26f383ac
ryo-murai <murai.ry@gmail.com>
2020-07-04 11:24:30 +0900

    Initial Commit

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by  ryo murai on 2020/07/04.
+//  Copyright © 2020 sample. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
