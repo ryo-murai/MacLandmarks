4c37d6a78c0a6537fe45daecdef734bd58931e1f
ryo-murai <murai.ry@gmail.com>
2020-07-07 21:13:39 +0900

    implement `compine views using stack`

--- MacLandmarks/ContentView.swift
+++ MacLandmarks/ContentView.swift
@@ -10,8 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
